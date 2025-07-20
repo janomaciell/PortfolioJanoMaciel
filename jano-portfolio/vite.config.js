323f0a3350c54bb9d53bcafbd65334a0ee3fe8e6
janomaciell <103576779+janomaciell@users.noreply.github.com>
2025-07-20 14:53:25 -0300

    AHORA SI

--- jano-portfolio/vite.config.js
+++ jano-portfolio/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/', // Importante: usa '/' en lugar de './'
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
